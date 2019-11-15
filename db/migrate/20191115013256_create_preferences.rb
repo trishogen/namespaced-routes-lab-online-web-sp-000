1bc590f25e9059ce4f9de61f30b750b03b4318e8
Trish Ogen <patriciaogen@gmail.com>
2019-11-15 01:34:02 +0000

    Automatically backed up by Learn

--- db/migrate/20191115013256_create_preferences.rb
+++ db/migrate/20191115013256_create_preferences.rb
@@ -1,6 +1,9 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
+
     end
   end
 end
